f5880af49215ed86003e538555925bce8de2c405
jsuizo <jayson.b.suizo@gmail.com>
2017-09-28 04:15:08 -0700

    update function managepurchase

---
+++ app/Http/Controllers/api/managePurchasesController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\userSubscription\userSubscriptionInterface as userSubscriptionInterface;
+use Illuminate\Support\Facades\Input;
+//use App\Http\Requests\manageSubscriptionRequest;
+
+class managePurchasesController extends Controller
+{   
+
+    public function __construct(userSubscriptionInterface $user_subscription){
+        $this->user_subscription = $user_subscription;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $search = [];
+        $offset = isset($_GET['offset']) ? $_GET['offset'] : 0;
+        $limit = isset($_GET['limit']) ? $_GET['limit'] : 10;
+
+        if(isset($_GET['all'])){
+            $search['all'] = true;
+        }
+
+        if(isset($_GET['user_id'])){
+            $search['user_id'] = $_GET['user_id'];
+        }
+
+        $data = $this->user_subscription->getAll($offset, $limit, $search);
+        $data['offset'] = isset($_GET['all']) ? 'all' :$offset;
+        $data['limit'] = isset($_GET['all']) ? 'all' : $limit;
+        $data['total'] = 0;
+        
+        $data['total'] = $this->user_subscription->count();
+
+        return response()->json(['success'=> $data ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
