0d51a5ea75d6e8a6a78b06380c4508522d4484bc
jsuizo <jayson.b.suizo@gmail.com>
2018-10-28 19:55:16 +0800

    update

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password','first_name','last_name','user_type','gender',
         'birth_date','profession_type','group_type','category','role','verification_code','new_email',
-        'email_verification_code','new_password','password_verification_code','archive','discharged_date'
+        'email_verification_code','new_password','password_verification_code','archive','discharged_date', 'note'
     ];
 
     /**
